8265426afe88c1208fcfa3ee158495d53d01805c
Anica <3c.innertherapy@gmail.com>
2026-04-10 22:36:42 +0100

    Update config.js

--- admin/config.js
+++ admin/config.js
@@ -14,6 +14,6 @@
     SUPABASE_URL:       'https://cgxjqsbrditbteqhdyus.supabase.co',
     SUPABASE_ANON_KEY:  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNneGpxc2JyZGl0YnRlcWhkeXVzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTExMTY1ODEsImV4cCI6MjA2NjY5MjU4MX0.xUDy5ic-r52kmRtocdcW8Np9-lczjMZ6YKPXc03rIG4',
     WORKER_URL:         'https://3c-card-showcase.3c-innertherapy.workers.dev',
-    SHOWCASE_BASE_URL:  'https://anica-blip.github.io/3c-card-showcase/landing.html',
+    SHOWCASE_BASE_URL:  'https://anica-blip.github.io/3C-Solo-Card-Showcase/landing.html',
   });
 })();
